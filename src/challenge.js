8ea5e3a790e55b2909a9bb90743e2314a70605e7
Phil Skaggs <prskaggsjr@gmail.com>
2016-06-09 14:52:04 -0600

    completed test two

--- src/challenge.js
+++ src/challenge.js
@@ -1,9 +1,5 @@
 module.exports.companies = function (rawData) {
-    // let result = [];
-    // let goodStuff = {
-    //   'name': companyName,
-    //   'employees': employeeArray
-    // };
+    let result = [];
 
     let people = rawData.people;
     let companies = rawData.companies;
@@ -17,19 +13,61 @@ module.exports.companies = function (rawData) {
 
 
     for (let i = 0; i < companies.length; i++) {
+      let goodStuff = new Object();
+      let peopleArr = [];
+      goodStuff.name = companies[i].name;
+      goodStuff.employees = peopleArr;
+
       for (var j = 0; j < employments.length; j++) {
         let employeeInfo = employments[j].employments
+
         for (var z = 0; z < employeeInfo.length; z++) {
           if (employeeInfo[z].company_id === companies[i].id) {
-            console.log('NAME!!!!!',companies[i].name);
+            let peopleStuff = new Object();
+            peopleStuff.id = employments[j].id;
+            peopleStuff.first_name = employments[j].first_name;
+            peopleStuff.last_name = employments[j].last_name;
+            peopleStuff.title = employeeInfo[z].title;
+            peopleArr.push(peopleStuff);
+          }
+        }
+      }
+      result.push(goodStuff);
+    }
+    return result;
+  };
+
+  module.exports.employments = function (rawData) {
+
+    let result = [];
+
+    let people = rawData.people;
+    let companies = rawData.companies;
+    let employees = people.map(function(num,index){
+      return people[index];
+    })
+
+    for (let i = 0; i < companies.length; i++) {
+
+      for (var j = 0; j < employees.length; j++) {
+
+        let employeeInfo = employees[j].employments
+
+        for (var z = 0; z < employeeInfo.length; z++) {
+
+          let goodStuff = new Object();
+          if (employeeInfo[z].company_id === companies[i].id) {
+
+            goodStuff.company_id = companies[i].id;
+            goodStuff.company_name = companies[i].name;
+            goodStuff.person_id = employees[j].id;
+            goodStuff.person_first_name = employees[j].first_name;
+            goodStuff.person_last_name = employees[j].last_name;
+            goodStuff.title = employeeInfo[z].title;
+            result.push(goodStuff);
           }
         }
       }
     }
-
-    // var employmentId = employments.map(function (num,index) {
-    //   return employments[index];
-    // })
-    //
-    // console.log(employments);
-  };
+    return result;
+  }
